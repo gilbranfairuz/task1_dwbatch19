0861f52a84876edc1d32069cdcd29c5dd8a4992e
septianmy <haiseptian@gmail.com>
2020-11-29 12:19:37 +0700

    Update Slider

--- src/components/molecules/slider/index.js
+++ src/components/molecules/slider/index.js
@@ -1,42 +1,22 @@
-import Carousel from 'react-bootstrap/Carousel'
+import OwlCarousel from 'react-owl-carousel';  
+import 'owl.carousel/dist/assets/owl.carousel.css';  
+import 'owl.carousel/dist/assets/owl.theme.default.css'; 
 import { Slider1 } from "../../../assets";
-
 import './slider.scss';
 
 const Slider = () => {
     return (
-        <Carousel>
-            <Carousel.Item interval={1000}>
-                <img
-                className="d-block w-100"
-                src={Slider1}
-                alt="First slide"
-                style={{
-                    height:"280px"
-                }}
-                />
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-                <img
-                className="d-block w-100"
-                src={Slider1}
-                alt="First slide"
-                style={{
-                    height:"280px"
-                }}
-                />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Slider1}
-                    alt="First slide"
-                    style={{
-                        height:"280px"
-                    }}
-                />
-            </Carousel.Item>
-            </Carousel>
+        <div class="slide">
+        <OwlCarousel items={1}  
+        className="owl-theme"  
+        loop
+        autoplay={true}
+        margin={25} >  
+            <div><img className="img" src="https://images.vexels.com/media/users/3/194731/raw/98a06d45de142b379f6d236526aa9ada-shop-online-web-slider-design.jpg"/></div>  
+            <div><img className="img" src="https://images.vexels.com/media/users/3/194733/raw/09e8c94ee143ff718334f2ddc0ecd330-online-shopping-neon-web-slider.jpg"/></div>
+            <div><img className="img" src="https://images.vexels.com/media/users/3/194698/preview2/34d9aa618f832510ce7290b4f183484a-shop-online-slider-template.jpg"/></div>
+        </OwlCarousel>
+        </div>  
     )
 }
 
